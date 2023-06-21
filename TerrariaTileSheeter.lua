55132df7261420822de27f3692c8be27c80324e1
Alex <axj30.11.04@gmail.com>
2023-06-21 21:11:28 +0200

    added horizontal slicing a

--- TerrariaTileSheeter.lua
+++ TerrariaTileSheeter.lua
@@ -38,7 +38,42 @@ if data.ok then
 
     local yData = tonumber(data.y)
 
-    app.alert(xData .. yData)
 
-    app.command.CanvasSize { ui = false, right = xData, bottom = yData } -- resizing canvas
+
+    --app.command.CanvasSize { ui = false, right = xData, bottom = yData } -- resizing canvas
+
+    local sprite = app.activeSprite
+    local currentImage = app.activeImage
+
+    local newSprite = Sprite(sprite.width + xData, sprite.height + yData)
+    local cel = newSprite:newCel(app.activeLayer, app.activeFrame)
+
+
+
+    local image = cel.image:clone()
+    for y = 0, sprite.height do
+
+        local xOffset = 0
+
+        local sliced = 0
+        for x = 0, sprite.width do
+            --print(y .." " .. x)
+
+            if x % 8 == 0 and x > 0 and sliced < xData then  --TODO fix this mess 
+                xOffset = xOffset + 1
+            end
+            local pixel = currentImage:getPixel(x - xOffset, y)
+            if sliced < xData and x % 8 == 0 and x > 0 then
+                pixel = 0
+                sliced = sliced + 1
+            end
+            image:drawPixel(x, y, pixel)
+            print("Drawing " .. pixel .. "at " .. x .. " " .. y)
+            print("Pixel taken from " .. x - xOffset )
+        end
+    end
+
+    cel.image = image
+    app.refresh()
+
 end
